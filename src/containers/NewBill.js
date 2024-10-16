a880c6e0eac54e61d58444610570810bdb10ad32
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-10-16 17:01:26 +0200

    Optimisation: Vérification du type de fichier dans la méthode handleChangeFile pour le Bug d'affichage des images

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -21,11 +21,12 @@ export default class NewBill {
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
     // Pour extraire la variable fileName
     const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
+    const fileName = filePath[filePath.length - 1]
 
     // Ajout de la vérification d'extension
-    const fileExtension = fileName.split('.').pop().toLowerCase();
-    if (!['jpg', 'jpeg', 'png'].includes(fileExtension)) {
+    // const fileExtension = fileName.split('.').pop().toLowerCase();
+    // if (!['jpg', 'jpeg', 'png'].includes(fileExtension)) {
+    if (!['image/jpeg', 'image/png'].includes(file.type)) {
       alert('Veuillez sélectionner un fichier image (jpg, jpeg, png)');
       return;
     }
@@ -43,7 +44,7 @@ export default class NewBill {
           noContentType: true
         }
       })
-      .then(({fileUrl, key}) => {
+      .then(({ fileUrl, key }) => {
         console.log(fileUrl)
         this.billId = key
         this.fileUrl = fileUrl
@@ -57,9 +58,9 @@ export default class NewBill {
     const bill = {
       email,
       type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
+      name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
       amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
-      date:  e.target.querySelector(`input[data-testid="datepicker"]`).value,
+      date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
       vat: e.target.querySelector(`input[data-testid="vat"]`).value,
       pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
       commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
@@ -75,12 +76,12 @@ export default class NewBill {
   updateBill = (bill) => {
     if (this.store) {
       this.store
-      .bills()
-      .update({data: JSON.stringify(bill), selector: this.billId})
-      .then(() => {
-        this.onNavigate(ROUTES_PATH['Bills'])
-      })
-      .catch(error => console.error(error))
+        .bills()
+        .update({ data: JSON.stringify(bill), selector: this.billId })
+        .then(() => {
+          this.onNavigate(ROUTES_PATH['Bills'])
+        })
+        .catch(error => console.error(error))
     }
   }
 }
