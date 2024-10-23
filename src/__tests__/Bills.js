f2ae5754c56c3d39de363575de78745aac1fefb5
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-10-23 16:57:37 +0200

    Modifications test d'intégration GET pour récupérer les factures

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -27,15 +27,15 @@ describe("Given I am connected as an employee", () => {
       await waitFor(() => screen.getByTestId("icon-window"));
       const windowIcon = screen.getByTestId("icon-window");
       //to-do write expect expression
-  // Vérifier que l'icône est surlignée (ex. vérifier une classe CSS comme 'active')
-  expect(windowIcon.classList.contains('active')).toBe(false);
+      // Vérifier que l'icône est surlignée (ex. vérifier une classe CSS comme 'active')
+      expect(windowIcon.classList.contains('active')).toBe(false);
     });
 
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills });
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML);
       const antiChrono = (a, b) => (a > b) ? 1 : -1;
-      const datesSorted = [ ...dates ].sort(antiChrono);
+      const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
     });
   });
@@ -85,13 +85,16 @@ describe("Given I am connected as an employee", () => {
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("fetches bills from mock API GET", async () => {
-      localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "e@e" }));
+      //   localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "e@e" }));
+      const billsSpy = jest.spyOn(mockStore, "bills"); // pour inspecter la méthode bills
       const root = document.createElement("div");
       root.setAttribute("id", "root");
       document.body.append(root);
       router();
       window.onNavigate(ROUTES_PATH.Bills);
+
       await waitFor(() => screen.getByText("Mes notes de frais"));
+      expect(billsSpy).toHaveBeenCalled(); // Pour vérifier si le mock a été appelé
       expect(screen.getByTestId("tbody")).toBeTruthy();
     });
   });
@@ -111,7 +114,7 @@ describe("When an error occurs on API", () => {
 
   test("fetches bills from an API and fails with 404 message error", async () => {
     mockStore.bills.mockImplementationOnce(() => {
-      return { list: () => { return Promise.reject(new Error("Erreur 404")); }};
+      return { list: () => { return Promise.reject(new Error("Erreur 404")); } };
     });
 
     window.onNavigate(ROUTES_PATH.Bills);
@@ -124,7 +127,7 @@ describe("When an error occurs on API", () => {
 // test d'intégration GET pour gérer l'erreur 500
 test("fetches bills from an API and fails with 500 message error", async () => {
   mockStore.bills.mockImplementationOnce(() => {
-    return { list: () => { return Promise.reject(new Error("Erreur 500")); }};
+    return { list: () => { return Promise.reject(new Error("Erreur 500")); } };
   });
 
   window.onNavigate(ROUTES_PATH.Bills);
