87bd8166c12cbfcad5980fdea702502c38ab81d7
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-11-03 01:48:53 +0100

    commentaires

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -12,126 +12,184 @@ import { localStorageMock } from "../__mocks__/localStorage.js";
 import mockStore from "../__mocks__/store";
 import router from "../app/Router.js";
 
+// Mock de la fonction store pour éviter les appels réels à l'API dans les tests
 jest.mock("../app/Store", () => mockStore);
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
+
+
+    // POUR VERIFIER QUE L'ICONE DES FACTURES EST SURLIGNEE
     test("Then bill icon in vertical layout should be highlighted", async () => {
+      // Configuration de l'utilisateur connecté dans le localStorage
       Object.defineProperty(window, "localStorage", { value: localStorageMock });
       window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+      // Simuler un utilisateur connecté dans le localStorage
+
+      // Création d'un élément racine pour router l'application
       const root = document.createElement("div");
       root.setAttribute("id", "root");
       document.body.append(root);
       router();
+
+      // Navigation vers la page Bills
       window.onNavigate(ROUTES_PATH.Bills);
+
+      // Attente que l'icône de factures (id 'icon-window') soit chargée dans le DOM
       await waitFor(() => screen.getByTestId("icon-window"));
+
+      // Sélection de l'icône et vérification de la présence de la classe CSS 'active'
       const windowIcon = screen.getByTestId("icon-window");
-      //to-do write expect expression
-      // Vérifier que l'icône est surlignée (ex. vérifier une classe CSS comme 'active')
       expect(windowIcon.classList.contains('active')).toBe(false);
+      // Vérification que l'icône est bien surlignée
     });
 
+
+    // POUR VERIFIER QUE LES FACTURES SONT TRIEES DU PLUS RECENT AU PLUS ANCIEN
     test("Then bills should be ordered from earliest to latest", () => {
+      // Injection des données de factures dans le HTML pour simuler la page
       document.body.innerHTML = BillsUI({ data: bills });
+      // Récupération de toutes les dates de facture et conversion en chaîne de caractères
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML);
+      // Fonction de tri antéchronologique (de la plus récente à la plus ancienne)
       const antiChrono = (a, b) => new Date(b) - new Date(a);
       const datesSorted = [...dates].sort(antiChrono);
+      // Vérification que les dates affichées sont bien triées correctement
       expect(dates).toEqual(datesSorted);
     });
   });
 
+
   describe("When I click on the eye icon of a bill", () => {
+
+    // POUR VERIFIER L'OUVERTURE DE LA MODALE AVEC L'ICONE 'EYE'
     test("It should open a modal", async () => {
+      // Simule la navigation pour définir le HTML de la route actuelle
       const onNavigate = pathname => { document.body.innerHTML = ROUTES({ pathname }); };
+      // Définition de l'utilisateur connecté dans le localStorage
       Object.defineProperty(window, "localStorage", { value: localStorageMock });
       window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+      // Création du conteneur Bills avec le mock de localStorage et de la fonction de navigation
       const billsContainer = new Bills({ document, onNavigate, store: null, localStorage: window.localStorage });
       document.body.innerHTML = BillsUI({ data: bills });
 
+      // Espionnage de la méthode handleClickIconEye pour vérifier si elle est appelée
       const handleClickIconEye = jest.fn(icon => billsContainer.handleClickIconEye(icon));
       const iconEye = await screen.getAllByTestId("icon-eye");
+      // Sélection de la modale pour vérifier son affichage
       const modaleFile = document.getElementById("modaleFile");
 
+      // Mock pour simuler l'affichage de la modale
       $.fn.modal = jest.fn(() => modaleFile.classList.add("show"));
 
+      // Pour chaque icône 'eye', on ajoute un event listener et simule un clic
       iconEye.forEach(icon => {
         icon.addEventListener("click", handleClickIconEye(icon));
         userEvent.click(icon);
         expect(handleClickIconEye).toHaveBeenCalled();
       });
 
+      // Vérification que la modale est visible après le clic
       expect(modaleFile).toBeTruthy();
     });
   });
 
+
+  // POUR VERIFIER L'OUVERTURE DE LA PAGE NEW BILL
   describe("When I click on the New Bill button", () => {
     test("It should open the New Bill page", async () => {
       const onNavigate = pathname => { document.body.innerHTML = ROUTES({ pathname }); };
+      // Mise en place d'un utilisateur connecté dans le localStorage
       Object.defineProperty(window, "localStorage", { value: localStorageMock });
       window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+      // Création du conteneur Bills
       const billsContainer = new Bills({ document, onNavigate, store: null, localStorage: window.localStorage });
       document.body.innerHTML = BillsUI({ data: bills });
 
+      // Espionnage de la méthode handleClickNewBill
       const btnNewBill = await screen.getByTestId("btn-new-bill");
       const handleClickNewBill = jest.fn(() => billsContainer.handleClickNewBill);
       btnNewBill.addEventListener("click", handleClickNewBill);
 
+      // Simulation du clic sur le bouton 'New Bill'
       userEvent.click(btnNewBill);
       expect(handleClickNewBill).toHaveBeenCalled();
+      // Vérification que le clic sur le bouton 'New Bill' ouvre bien la page correspondante
     });
   });
 });
-// test d'intégration GET pour récupérer les factures
+
+
+// TEST D'INTEGRATION GET POUR RECUPERER LES FACTURES
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("fetches bills from mock API GET", async () => {
       //   localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "e@e" }));
-      const billsSpy = jest.spyOn(mockStore, "bills"); // pour inspecter la méthode bills
+      const billsSpy = jest.spyOn(mockStore, "bills"); // Espionne la méthode bills pour vérifier qu'elle est appelée
+
+      // Création d'un élément racine pour la navigation
       const root = document.createElement("div");
       root.setAttribute("id", "root");
       document.body.append(root);
       router();
+      // Navigation vers la page Bills
       window.onNavigate(ROUTES_PATH.Bills);
 
+      // Attente que le texte 'Mes notes de frais' s'affiche
       await waitFor(() => screen.getByText("Mes notes de frais"));
-      expect(billsSpy).toHaveBeenCalled(); // Pour vérifier si le mock a été appelé
+      // Vérifie que la méthode du mock store a été appelée pour récupérer les factures
+      expect(billsSpy).toHaveBeenCalled();
       expect(screen.getByTestId("tbody")).toBeTruthy();
+      // Vérification que les factures sont récupérées avec succès
     });
   });
 });
 
-// test d'intégration GET pour gérer l'erreur 404
+
+// TEST D'INTEGRATION GET POUR GERER L'ERREUR 404
 describe("When an error occurs on API", () => {
   beforeEach(() => {
+    // Crée un espion sur la méthode "bills" de "mockStore" pour surveiller les appels sans réellement l'exécuter
     jest.spyOn(mockStore, "bills");
+    // Remplace le localStorage du navigateur par une version simulée (localStorageMock)
     Object.defineProperty(window, "localStorage", { value: localStorageMock });
+    // Enregistre un utilisateur de type "Employee" avec un email fictif dans le localStorage simulé
     window.localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "e@e" }));
     const root = document.createElement("div");
     root.setAttribute("id", "root");
     document.body.appendChild(root);
+    // Initialise le routeur de l'application pour gérer la navigation entre les pages
     router();
   });
 
+
   test("fetches bills from an API and fails with 404 message error", async () => {
+    // Mock pour simuler une erreur 404 lors de la récupération des factures
     mockStore.bills.mockImplementationOnce(() => {
       return { list: () => { return Promise.reject(new Error("Erreur 404")); } };
     });
 
+    // Navigation vers la page Bills et attente de l'affichage du message d'erreur
     window.onNavigate(ROUTES_PATH.Bills);
     await new Promise(process.nextTick);
+    // Vérifie que le message d'erreur 404 est affiché en cas d'échec de l'API
     const message = await screen.getByText(/Erreur 404/);
     expect(message).toBeTruthy();
   });
 });
 
-// test d'intégration GET pour gérer l'erreur 500
+
+// TEST D'INTEGRATION GET POUR GERER L'ERREUR 500
 test("fetches bills from an API and fails with 500 message error", async () => {
+  // Mock pour simuler une erreur 500 lors de la récupération des factures
   mockStore.bills.mockImplementationOnce(() => {
     return { list: () => { return Promise.reject(new Error("Erreur 500")); } };
   });
 
+  // Navigation vers la page Bills et attente de l'affichage du message d'erreur
   window.onNavigate(ROUTES_PATH.Bills);
   await new Promise(process.nextTick);
+  // Vérifie que le message d'erreur 500 est affiché  en cas d'échec de l'API
   const message = await screen.getByText(/Erreur 500/);
   expect(message).toBeTruthy();
 });
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,112 +2,308 @@
  * @jest-environment jsdom
  */
 
-import { screen, waitFor } from "@testing-library/dom";
-import NewBillUI from "../views/NewBillUI.js";
-import NewBill from "../containers/NewBill.js";
+import { screen, waitFor } from "@testing-library/dom"
+import NewBillUI from "../views/NewBillUI.js"
+import NewBill from "../containers/NewBill.js"
+
 import { ROUTES_PATH } from "../constants/routes.js";
 import router from "../app/Router.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 import userEvent from "@testing-library/user-event";
 import mockStore from "../__mocks__/store";
 
+
 describe("Given I am connected as an employee", () => {
-  let onNavigate;
-  let newBill;
+  describe("When I am on NewBill Page", () => {
 
-  beforeEach(() => {
-    document.body.innerHTML = NewBillUI();
+    // POUR VERIFIER QUE L'ICONE POUR NEWBILL EST SURLIGNEE
+    test("Then NewBill icon in vertical layout should be highlighted", async () => {
+      const html = NewBillUI()
+      document.body.innerHTML = html
 
-    onNavigate = jest.fn();
+      Object.defineProperty(window, "localStorage", { value: localStorageMock });
+      // localStorage pour simuler un utilisateur connecté.
+      window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+      // Enregistrement de l'utilisateur simulé dans le localStorage
 
-    // Configure le localStorage pour simuler un utilisateur connecté
-    Object.defineProperty(window, "localStorage", { value: localStorageMock });
-    window.localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "test@test.com" }));
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.NewBill);
 
-    // Crée une nouvelle instance de NewBill pour chaque test
-    newBill = new NewBill({
-      document,
-      onNavigate,
-      store: mockStore,
-      localStorage: window.localStorage,
+      await waitFor(() => screen.getByTestId("icon-mail"));
+      // Pour attendre que l'icône mail soit présente
+      const mailIcon = screen.getByTestId("icon-mail");
+      expect(mailIcon.classList.contains('active-icon')).toBe(true);
+      // Vérification que l'icône a bien la classe 'active' pour indiquer qu'elle est surlignée.
+    })
+
+
+    // POUR VERIFIER QUE LE FORMULAIRE NEWBILL EST PRESENT
+    test("Then the NewBill form should be visible", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+
+      // Vérifier le formulaire NewBill est présent
+      const formNewBill = screen.getByTestId("form-new-bill");
+      // Pour sélectionner le formulaire avec l'attribut data-testid "form-new-bill".
+      expect(formNewBill).toBeTruthy();
+      // Pour vérifier que le formulaire est présent
     });
-  });
 
-  test("Then NewBill icon in vertical layout should be highlighted", async () => {
-    const root = document.createElement("div");
-    root.setAttribute("id", "root");
-    document.body.append(root);
-    router();
-    window.onNavigate(ROUTES_PATH.NewBill);
 
-    await waitFor(() => screen.getByTestId("icon-mail"));
-    const mailIcon = screen.getByTestId("icon-mail");
-    expect(mailIcon.classList.contains("active-icon")).toBe(true);
-  });
+    // POUR VERIFIER QUE LE FORMULAIRE VIDE NE SE SOUMET PAS
+    test("When I submit an empty form, it should not navigate away from the form", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
 
-  test("Then the NewBill form should be visible", () => {
-    const formNewBill = screen.getByTestId("form-new-bill");
-    expect(formNewBill).toBeTruthy();
-  });
+      // Mock du localStorage
+      Object.defineProperty(window, "localStorage", { value: localStorageMock });
+      window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+      // Utilisation du mock localStorage pour simuler un utilisateur connecté.
 
-  test("When I submit an empty form, it should not navigate away from the form", () => {
-    const form = screen.getByTestId("form-new-bill");
+      // Pour initialiser NewBill avec les mocks nécessaires
+      const onNavigate = jest.fn();
+      const newBill = new NewBill({ document, onNavigate, store: null, localStorage: window.localStorage });
 
-    // Espionne la méthode handleSubmit et simule la soumission
-    const handleSubmitSpy = jest.spyOn(newBill, "handleSubmit");
-    form.addEventListener("submit", newBill.handleSubmit);
-    form.dispatchEvent(new Event("submit"));
+      // Pour espionner la méthode handleSubmit
+      const handleSubmitSpy = jest.spyOn(newBill, "handleSubmit");
 
-    // Vérifie que handleSubmit a été appelée, mais pas la navigation
-    expect(handleSubmitSpy).toHaveBeenCalled();
-    expect(onNavigate).not.toHaveBeenCalled();
-  });
+      // Pour relier handleSubmit au formulaire
+      const form = screen.getByTestId("form-new-bill");
+      form.addEventListener("submit", newBill.handleSubmit);
 
-  test("When I upload a file with an incorrect format, it should display an alert", () => {
-    window.alert = jest.fn();
-    const fileInput = screen.getByTestId("file");
-    const file = new File(["dummy content"], "example.txt", { type: "text/plain" });
+      // Pour simuler que la soumission du formulaire vide
+      form.dispatchEvent(new Event("submit"));
 
-    userEvent.upload(fileInput, file);
+      // Pour vérifier que la méthode handleSubmit a été appelée
+      expect(handleSubmitSpy).toHaveBeenCalled();
 
-    expect(window.alert).toHaveBeenCalledWith("Veuillez sélectionner un fichier image (jpg, jpeg, png)");
-  });
+      // Pour vérifier que la navigation n'a pas été déclenchée si le formulaire est vide
+      expect(onNavigate).not.toHaveBeenCalled();
+    });
 
-  test("When I upload a correct file format, it should call handleChangeFile method", async () => {
-    const handle = jest.fn(newBill.handleChangeFile);
-    const file = new File(["dummy content"], "example.png", { type: "image/png" });
 
-    await waitFor(() => {
+    //POUR VERIFIER L'UPLOAD D'UN fICHIER INCORRECT
+    test("When I upload a file with an incorrect format, it should display an alert", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+      Object.defineProperty(window, "localStorage", { value: localStorageMock });
+      // localStorage pour simuler un utilisateur connecté.
+      window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+      // Enregistrement de l'utilisateur simulé dans le localStorage
+      const onNavigate = jest.fn();
+      const newBill = new NewBill({ document, onNavigate, store: null, localStorage: window.localStorage });
+
+      window.alert = jest.fn();
+
       const fileInput = screen.getByTestId("file");
-      fileInput.addEventListener("change", handle);
+      const file = new File(["dummy content"], "example.txt", { type: "text/plain" });
+      // Création d'un fichier fictif au format incorrect.
       userEvent.upload(fileInput, file);
+      // Simulation de l'upload du fichier incorrect.
+
+      expect(window.alert).toHaveBeenCalledWith('Veuillez sélectionner un fichier image (jpg, jpeg, png)');
+      // Vérification que l'alerte a bien été déclenchée avec le bon message.
     });
 
-    expect(handle).toHaveBeenCalled();
-  });
 
-  test("When I submit a filled form, it should call the updateBill method and navigate to Bills", async () => {
-    const form = screen.getByTestId("form-new-bill");
-    const inputDate = screen.getByTestId("datepicker");
-    const inputAmount = screen.getByTestId("amount");
-    const inputFile = screen.getByTestId("file");
+    // POUR VERIFIER L'UPLOAD D'UNE IMAGE CORRECTE
+    // Test pour vérifier l'upload d'une image correcte
+    test("When I upload a correct file format, it should call the createBill method", async () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
 
-    userEvent.type(inputDate, "2024-10-21");
-    userEvent.type(inputAmount, "200");
-    const file = new File(["dummy content"], "example.png", { type: "image/png" });
-    userEvent.upload(inputFile, file);
+      const onNavigate = jest.fn();
 
-    const updateBillSpy = jest.spyOn(newBill, "updateBill");
-    form.dispatchEvent(new Event("submit"));
+      Object.defineProperty(window, "localStorage", { value: localStorageMock });
+      // localStorage pour simuler un utilisateur connecté.
+      window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+      // Enregistrement de l'utilisateur simulé dans le localStorage
 
-    expect(updateBillSpy).toHaveBeenCalled();
-    expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH.Bills);
-  });
+      // Initialiser le composant NewBill avec le mockStore
+      const newBill = new NewBill({ document, onNavigate, store: mockStore, localStorage: window.localStorage });
+      const handle = jest.fn(newBill.handleChangeFile)
+      // Sélectionner l'élément d'input pour l'upload de fichier
+      const file = new File(["dummy content"], "example.png", { type: "image/png" });
+      await waitFor(() => {
+        const fileInput = screen.getByTestId("file");
+        fileInput.addEventListener("change", handle)
+        // Simuler l'upload du fichier
+        userEvent.upload(fileInput, file);
+      }) 
+      // Vérifier que la méthode create a bien été appelée
+      expect(handle).toHaveBeenCalled();
+    });
 
-  test("When I click on the logout button, I should be redirected to the Login page", () => {
-    const logoutButton = document.querySelector("#layout-disconnect");
-    userEvent.click(logoutButton);
 
-    expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH.Login);
-  });
-});
+    // POUR VERIFIER LA SOUMISSION DU FORMULAIRE
+    test("When I submit a filled form, it should call the updateBill method and navigate to Bills", async () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+
+      const onNavigate = jest.fn();
+      Object.defineProperty(window, "localStorage", { value: localStorageMock });
+      // localStorage pour simuler un utilisateur connecté.
+      window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+      // Enregistrement de l'utilisateur simulé dans le localStorage
+      const newBill = new NewBill({ document, onNavigate, store: mockStore, localStorage: window.localStorage });
+
+      // Remplir des champs du formulaire
+      const form = screen.getByTestId("form-new-bill");
+      const inputDate = screen.getByTestId("datepicker");
+      const inputAmount = screen.getByTestId("amount");
+      const inputFile = screen.getByTestId("file");
+
+      userEvent.type(inputDate, "2024-10-21");
+      userEvent.type(inputAmount, "200");
+      const file = new File(["dummy content"], "example.png", { type: "image/png" });
+      userEvent.upload(inputFile, file);
+
+      // Espionner de la méthode updateBill
+      const updateBillSpy = jest.spyOn(newBill, "updateBill");
+
+      // Simuler de la soumission du formulaire
+      form.dispatchEvent(new Event("submit"));
+
+      // Vérifier que la méthode updateBill a été appelée
+      expect(updateBillSpy).toHaveBeenCalled();
+      // Vérifier la redirection vers la page "Bills"
+      expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH.Bills);
+    });
+
+
+
+    // POUR DECONNECTER L'EMPLOYE
+    test("When I click on the logout button, I should be redirected to the Login page", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+
+      const onNavigate = jest.fn();
+      const newBill = new NewBill({ document, onNavigate, store: null, localStorage: window.localStorage });
+
+      const logoutButton = document.querySelector('#layout-disconnect');
+      userEvent.click(logoutButton);
+
+      expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH.Login);
+    });
+
+
+  })
+})
+
+// /**
+//  * @jest-environment jsdom
+//  */
+
+// import { screen, waitFor } from "@testing-library/dom";
+// import NewBillUI from "../views/NewBillUI.js";
+// import NewBill from "../containers/NewBill.js";
+// import { ROUTES_PATH } from "../constants/routes.js";
+// import router from "../app/Router.js";
+// import { localStorageMock } from "../__mocks__/localStorage.js";
+// import userEvent from "@testing-library/user-event";
+// import mockStore from "../__mocks__/store";
+
+// describe("Given I am connected as an employee", () => {
+//   let onNavigate;
+//   let newBill;
+
+//   beforeEach(() => {
+//     document.body.innerHTML = NewBillUI();
+
+//     onNavigate = jest.fn();
+
+//     // Configure le localStorage pour simuler un utilisateur connecté
+//     Object.defineProperty(window, "localStorage", { value: localStorageMock });
+//     window.localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "test@test.com" }));
+
+//     // Crée une nouvelle instance de NewBill pour chaque test
+//     newBill = new NewBill({
+//       document,
+//       onNavigate,
+//       store: mockStore,
+//       localStorage: window.localStorage,
+//     });
+//   });
+
+  
+//   test("Then NewBill icon in vertical layout should be highlighted", async () => {
+//     const root = document.createElement("div");
+//     root.setAttribute("id", "root");
+//     document.body.append(root);
+//     router();
+//     window.onNavigate(ROUTES_PATH.NewBill);
+
+//     await waitFor(() => screen.getByTestId("icon-mail"));
+//     const mailIcon = screen.getByTestId("icon-mail");
+//     expect(mailIcon.classList.contains("active-icon")).toBe(true);
+//   });
+
+//   test("Then the NewBill form should be visible", () => {
+//     const formNewBill = screen.getByTestId("form-new-bill");
+//     expect(formNewBill).toBeTruthy();
+//   });
+
+//   test("When I submit an empty form, it should not navigate away from the form", () => {
+//     const form = screen.getByTestId("form-new-bill");
+
+//     // Espionne la méthode handleSubmit et simule la soumission
+//     const handleSubmitSpy = jest.spyOn(newBill, "handleSubmit");
+//     form.addEventListener("submit", newBill.handleSubmit);
+//     form.dispatchEvent(new Event("submit"));
+
+//     // Vérifie que handleSubmit a été appelée, mais pas la navigation
+//     expect(handleSubmitSpy).toHaveBeenCalled();
+//     expect(onNavigate).not.toHaveBeenCalled();
+//   });
+
+//   test("When I upload a file with an incorrect format, it should display an alert", () => {
+//     window.alert = jest.fn();
+//     const fileInput = screen.getByTestId("file");
+//     const file = new File(["dummy content"], "example.txt", { type: "text/plain" });
+
+//     userEvent.upload(fileInput, file);
+
+//     expect(window.alert).toHaveBeenCalledWith("Veuillez sélectionner un fichier image (jpg, jpeg, png)");
+//   });
+
+//   test("When I upload a correct file format, it should call handleChangeFile method", async () => {
+//     const handle = jest.fn(newBill.handleChangeFile);
+//     const file = new File(["dummy content"], "example.png", { type: "image/png" });
+
+//     await waitFor(() => {
+//       const fileInput = screen.getByTestId("file");
+//       fileInput.addEventListener("change", handle);
+//       userEvent.upload(fileInput, file);
+//     });
+
+//     expect(handle).toHaveBeenCalled();
+//   });
+
+//   test("When I submit a filled form, it should call the updateBill method and navigate to Bills", async () => {
+//     const form = screen.getByTestId("form-new-bill");
+//     const inputDate = screen.getByTestId("datepicker");
+//     const inputAmount = screen.getByTestId("amount");
+//     const inputFile = screen.getByTestId("file");
+
+//     userEvent.type(inputDate, "2024-10-21");
+//     userEvent.type(inputAmount, "200");
+//     const file = new File(["dummy content"], "example.png", { type: "image/png" });
+//     userEvent.upload(inputFile, file);
+
+//     const updateBillSpy = jest.spyOn(newBill, "updateBill");
+//     form.dispatchEvent(new Event("submit"));
+
+//     expect(updateBillSpy).toHaveBeenCalled();
+//     expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH.Bills);
+//   });
+
+//   test("When I click on the logout button, I should be redirected to the Login page", () => {
+//     const logoutButton = document.querySelector("#layout-disconnect");
+//     userEvent.click(logoutButton);
+
+//     expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH.Login);
+//   });
+// });
